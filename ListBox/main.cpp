645c44121c6b49f3f6cbb0039ada7248ed7151d7
Alexander Melikhov <alexandersermel@gmail.com>
2024-10-07 21:17:10 +0300

    ListBox_done

---
+++ ListBox/main.cpp
@@ -0,0 +1,99 @@
+//ListBox
+#include<Windows.h>
+#include"resource.h"
+#include<cstdio>
+#define _CRT_SECURE_NO_WARNINGS
+
+CONST CHAR* g_LIST_BOX_ITEMS[] = { "This", "Is", "My","First","ListBox" };
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR ipCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL,(DLGPROC) DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hCombo = GetDlgItem(hwnd, IDC_LIST1);
+		for (int i = 0; i < sizeof(g_LIST_BOX_ITEMS) / sizeof(g_LIST_BOX_ITEMS[0]); i++)
+		{
+			SendMessage(hCombo, LB_ADDSTRING, 0, (LPARAM)g_LIST_BOX_ITEMS[i]);
+		}
+	}
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case  IDC_BUTTON_ADD:
+			//GETMODULEHANDLE(NULL)аозвращает hInstance запущенной программы
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD_ITEM), hwnd, (DLGPROC)DlgProcAdd, 0);
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			CHAR sz_message[SIZE]{};
+			HWND hCombo = GetDlgItem(hwnd, IDC_LIST1);
+			INT i = SendMessage(hCombo, LB_GETCURSEL, 0, 0);
+			
+			if(i!=LB_ERR)
+			sprintf_s(sz_message,"Вы выбрали элемент №%i со значением %s",i,sz_buffer);
+
+			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+
+		}
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+
+	}
+	return FALSE;
+
+
+}
+
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT_NAME), WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			HWND hParent = GetParent(hwnd); //получаем родительское окно
+			HWND hListBox = GetDlgItem(hParent, IDC_LIST1);
+			SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			if (SendMessage(hListBox, LB_FINDSTRING, -1, (LPARAM)sz_buffer) == LB_ERR)
+				SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			else
+				MessageBox(hwnd, "Такое вхождение уже существует", "info", MB_OK | MB_ICONINFORMATION);
+
+		}
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+
+	}
+	return FALSE;
+
+
+}
